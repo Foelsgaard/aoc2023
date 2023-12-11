afca92dadd61225a8e05e9fef670e7154835364c
Johan Følsgaard <johan@foelsgaard.eu>
2023-12-11 22:17:12 +0100

    Simplify day10 solution
    Using shoelace formula instead of flood fill.

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -10,209 +10,71 @@ fn main() {
     println!("10b: {b}");
 }
 
-fn solve(input: &mut [u8]) -> (usize, usize) {
-    const GRID_LEN: usize = 0x200;
-
-    let mut grid = [0_u8; GRID_LEN * GRID_LEN];
-
+fn solve(input: &mut [u8]) -> (isize, isize) {
     let row_len = Parser::new(input).skip_line().unwrap().len();
     let start = input.iter().position(|b| *b == b'S').unwrap();
 
-    let left_horz = matches!(input.get(start - 1), Some(b'-') | Some(b'L') | Some(b'F'));
-    let right_horz = matches!(input.get(start + 1), Some(b'-') | Some(b'J') | Some(b'7'));
-    let up_vert = matches!(
-        input.get(start - row_len),
-        Some(b'|') | Some(b'F') | Some(b'7')
-    );
-    let down_vert = matches!(
-        input.get(start + row_len),
-        Some(b'|') | Some(b'L') | Some(b'J')
-    );
+    let left = input
+        .get(start - 1)
+        .map_or(false, |b| matches!(b, b'-' | b'L' | b'F'));
+    let right = input
+        .get(start + 1)
+        .map_or(false, |b| matches!(b, b'-' | b'J' | b'7'));
+    let up = input
+        .get(start - row_len)
+        .map_or(false, |b| matches!(b, b'|' | b'F' | b'7'));
+    let down = input
+        .get(start + row_len)
+        .map_or(false, |b| matches!(b, b'|' | b'L' | b'J'));
 
-    let (start_row, start_col) = ix2sub(row_len, start);
+    let width = row_len as isize;
 
-    let mut ixs;
-    *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 1, start_col * 2 + 1)
-        .unwrap()
-        .0 = 1;
-    if left_horz && right_horz {
-        input[start] = b'-';
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 1, start_col * 2)
-            .unwrap()
-            .0 = 1;
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 1, start_col * 2 + 2)
-            .unwrap()
-            .0 = 1;
-        ixs = [start - 1, start + 1];
-    } else if up_vert && down_vert {
-        input[start] = b'|';
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2, start_col * 2 + 1)
-            .unwrap()
-            .0 = 1;
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 2, start_col * 2 + 1)
-            .unwrap()
-            .0 = 1;
-        ixs = [start - row_len, start + row_len];
-    } else if up_vert && right_horz {
-        input[start] = b'F';
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2, start_col * 2 + 1)
-            .unwrap()
-            .0 = 1;
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 1, start_col * 2 + 2)
-            .unwrap()
-            .0 = 1;
-        ixs = [start - row_len, start + 1];
-    } else if down_vert && right_horz {
-        input[start] = b'L';
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 2, start_col * 2 + 1)
-            .unwrap()
-            .0 = 1;
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 1, start_col * 2 + 2)
-            .unwrap()
-            .0 = 1;
-        ixs = [start + row_len, start + 1];
-    } else if up_vert && left_horz {
-        input[start] = b'J';
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2, start_col * 2 + 1)
-            .unwrap()
-            .0 = 1;
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 1, start_col * 2)
-            .unwrap()
-            .0 = 1;
-        ixs = [start - row_len, start - 1];
-    } else if down_vert && left_horz {
-        input[start] = b'7';
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 2, start_col * 2 + 1)
-            .unwrap()
-            .0 = 1;
-        *by_sub_mut(&mut grid, GRID_LEN, start_row * 2 + 1, start_col * 2)
-            .unwrap()
-            .0 = 1;
-        ixs = [start + row_len, start - 1];
-    } else {
-        dbg!(left_horz, right_horz, up_vert, down_vert);
-        panic!();
-    }
+    let connections = [
+        (left, [-1, 0]),
+        (right, [1, 0]),
+        (up, [0, -1]),
+        (down, [0, 1]),
+    ];
 
-    let mut prevs = [start, start];
+    let [mut dx, mut dy] = connections
+        .into_iter()
+        .find_map(|(connected, dir)| connected.then_some(dir))
+        .unwrap();
 
-    let mut sum1 = 1;
+    let mut perimeter = 0;
+    let mut area = 0;
+
+    let [x0, y0] = [(start as isize) % width, (start as isize) / width];
+    let [mut x, mut y] = [x0, y0];
+
     loop {
-        sum1 += 1;
-        for (ix, prev) in ixs.iter_mut().zip(prevs.iter_mut()) {
-            let (row, col) = ix2sub(row_len, *ix);
+        let [x1, y1] = [x + dx, y + dy];
 
-            let current = input[*ix];
+        // Shoelace formula
+        area += y * x1 - y1 * x;
 
-            let ds: [(isize, isize); 2] = match current {
-                b'|' => [(-1, 0), (1, 0)],
-                b'-' => [(0, -1), (0, 1)],
-                b'L' => [(-1, 0), (0, 1)],
-                b'J' => [(-1, 0), (0, -1)],
-                b'7' => [(0, -1), (1, 0)],
-                b'F' => [(0, 1), (1, 0)],
-                b => panic!("unexpected character {}", b as char),
-            };
+        perimeter += 1;
 
-            'outer: for (drow, dcol) in ds {
-                if let Some(next) = sub2ix(row_len, row + drow, col + dcol) {
-                    if next == *prev {
-                        continue;
-                    }
-
-                    match input.get(next) {
-                        Some(b'|') if drow != 0 && dcol == 0 => {}
-                        Some(b'-') if drow == 0 && dcol != 0 => {}
-                        Some(b'L') if drow == 1 || dcol == -1 => {}
-                        Some(b'J') if drow == 1 || dcol == 1 => {}
-                        Some(b'7') if drow == -1 || dcol == 1 => {}
-                        Some(b'F') if drow == -1 || dcol == -1 => {}
-                        Some(b'.') | Some(b'#') | None => continue,
-                        Some(b) => panic!("unexpected character {}", *b as char),
-                    }
-                    input[*prev] = b'#';
-                    *prev = *ix;
-                    *ix = next;
-
-                    *by_sub_mut(&mut grid, GRID_LEN, row * 2 + 1, col * 2 + 1)
-                        .unwrap()
-                        .0 = 1;
-                    *by_sub_mut(
-                        &mut grid,
-                        GRID_LEN,
-                        (row + drow) * 2 + 1,
-                        (col + dcol) * 2 + 1,
-                    )
-                    .unwrap()
-                    .0 = 1;
-                    *by_sub_mut(&mut grid, GRID_LEN, row * 2 + drow + 1, col * 2 + dcol + 1)
-                        .unwrap()
-                        .0 = 1;
-
-                    break 'outer;
-                }
-            }
+        let next = y1 * width + x1;
+        match input[next as usize] {
+            b'L' | b'7' => [dx, dy] = [dy, dx],
+            b'J' | b'F' => [dx, dy] = [-dy, -dx],
+            _ => {}
         }
 
-        if ixs[0] == ixs[1] {
+        [x, y] = [x1, y1];
+
+        if [x, y] == [x0, y0] {
             break;
         }
     }
 
-    let mut stack = [0; 0x20000];
-    let mut stack_len = 1;
+    // Shoelace gives twice the area and includes the perimeter
+    //
+    // For some reason I don't fully understand, this always gives a result
+    // that is one less than the correct amount, so we add one in the end..
+    // This works for every test I've thrown against it.
+    area = (area.abs() - perimeter) / 2 + 1;
 
-    while stack_len > 0 {
-        let current = stack[stack_len - 1];
-        stack_len -= 1;
-
-        let (row, col) = ix2sub(GRID_LEN, current);
-
-        for (drow, dcol) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-            if let Some((next, ix)) = by_sub_mut(&mut grid, GRID_LEN, row + drow, col + dcol) {
-                if *next == 0 {
-                    *next = 2;
-                    stack[stack_len] = ix;
-                    stack_len += 1;
-                }
-            }
-        }
-    }
-
-    let mut sum2 = 0;
-
-    for (ix, b) in input.iter().enumerate() {
-        let (row, col) = ix2sub(row_len, ix);
-        if *b == b'\n' {
-            continue;
-        }
-
-        if let Some((0, _)) = by_sub(&grid, GRID_LEN, row * 2 + 1, col * 2 + 1) {
-            sum2 += 1;
-        }
-    }
-
-    (sum1, sum2)
-}
-
-fn ix2sub(row_len: usize, ix: usize) -> (isize, isize) {
-    let row = (ix / row_len) as isize;
-    let col = (ix % row_len) as isize;
-
-    (row, col)
-}
-
-fn sub2ix(row_len: usize, row: isize, col: isize) -> Option<usize> {
-    if row < 0 || row_len <= (col as usize) || col < 0 {
-        return None;
-    }
-    Some((row * (row_len as isize) + col) as usize)
-}
-
-fn by_sub<T>(ar: &[T], row_len: usize, row: isize, col: isize) -> Option<(&T, usize)> {
-    sub2ix(row_len, row, col).and_then(|ix| Some((ar.get(ix)?, ix)))
-}
-
-fn by_sub_mut<T>(ar: &mut [T], row_len: usize, row: isize, col: isize) -> Option<(&mut T, usize)> {
-    sub2ix(row_len, row, col).and_then(|ix| Some((ar.get_mut(ix)?, ix)))
+    (perimeter / 2, area)
 }
