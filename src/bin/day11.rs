93ec941f388dbad575c9e10b2b30477d401001d8
Johan Følsgaard <johan@foelsgaard.eu>
2023-12-11 22:16:52 +0100

    Simplify day11 solution

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -1,116 +1,62 @@
-use aoc2023::{read_input, Grid, Parser};
+use aoc2023::{read_input, Parser};
 
 fn main() {
     let mut buf = vec![0; 0x10000];
     let input = read_input(&mut buf);
 
-    let (a, b) = solve(input);
+    let a = solve::<2>(input);
+    let b = solve::<1000000>(input);
 
     println!("11a: {a}");
     println!("11b: {b}");
 }
 
-fn solve(input: &mut [u8]) -> (usize, usize) {
+fn solve<const K: usize>(input: &mut [u8]) -> usize {
     let mut parser = Parser::new(input);
 
-    let mut stars = [0; 0x1000];
+    let mut rowsum = [K; 0x100];
+    let mut colsum = [K; 0x100];
+
+    let mut stars = [(0, 0); 0x1000];
     let mut stars_len = 0;
 
     let mut width = 0;
     let mut height = 0;
     let mut ix = 0;
     while let Some(line) = parser.skip_line() {
+        width = line.len();
         for b in line {
+            let row = ix / width;
+            let col = ix % width;
+
             if *b == b'#' {
-                stars[stars_len] = ix;
+                stars[stars_len] = (row, col);
                 stars_len += 1;
+                rowsum[row] = 1;
+                colsum[col] = 1;
             }
 
             ix += 1;
         }
 
-        width = line.len();
         height += 1;
     }
 
-    let mut grid = Grid::new(width, input);
+    for i in 1..width {
+        rowsum[i] += rowsum[i - 1];
+    }
+    for i in 1..height {
+        colsum[i] += colsum[i - 1];
+    }
 
-    for row in 0..height as isize {
-        let mut is_empty = true;
-        for col in 0..width as isize {
-            if let Some(entry) = grid.get_mut(row, col) {
-                is_empty &= *entry != b'#';
-            }
-        }
+    let mut sum = 0;
 
-        if is_empty {
-            for col in 0..width as isize {
-                if let Some(entry) = grid.get_mut(row, col) {
-                    if *entry != b'\n' {
-                        *entry = b'-';
-                    }
-                }
-            }
+    for (n, (r0, c0)) in stars.iter().enumerate().take(stars_len) {
+        for (r1, c1) in stars.iter().skip(n + 1).take(stars_len - n - 1) {
+            sum += rowsum[*r0.max(r1)] - rowsum[*r0.min(r1)];
+            sum += colsum[*c0.max(c1)] - colsum[*c0.min(c1)];
         }
     }
 
-    for col in 0..width as isize {
-        let mut is_empty = true;
-        for row in 0..height as isize {
-            if let Some(entry) = grid.get_mut(row, col) {
-                is_empty &= *entry != b'#';
-            }
-        }
-
-        if is_empty {
-            for row in 0..width as isize {
-                if let Some(entry) = grid.get_mut(row, col) {
-                    if *entry == b'-' {
-                        *entry = b'+';
-                    } else if *entry != b'\n' {
-                        *entry = b'|';
-                    }
-                }
-            }
-        }
-    }
-
-    let mut sum1 = 0;
-    let mut sum2 = 0;
-    for (n, star0) in stars.iter().take(stars_len).enumerate() {
-        for star1 in stars.iter().skip(n + 1).take(stars_len - n - 1) {
-            let (mut r0, mut c0) = grid.sub_of(*star0);
-            let (r1, c1) = grid.sub_of(*star1);
-
-            let mut distance0: usize = 0;
-            let mut distance1: usize = 0;
-            while r0 != r1 {
-                r0 += (r1 - r0).signum();
-                let entry = grid.get(r0, c0).unwrap();
-                if *entry == b'-' || *entry == b'+' {
-                    distance0 += 2;
-                    distance1 += 1000000;
-                } else {
-                    distance0 += 1;
-                    distance1 += 1;
-                }
-            }
-            while c0 != c1 {
-                c0 += (c1 - c0).signum();
-                let entry = grid.get(r0, c0).unwrap();
-                if *entry == b'|' || *entry == b'+' {
-                    distance0 += 2;
-                    distance1 += 1000000;
-                } else {
-                    distance0 += 1;
-                    distance1 += 1;
-                }
-            }
-
-            sum1 += distance0;
-            sum2 += distance1;
-        }
-    }
-
-    (sum1, sum2)
+    sum
 }
